97f93cab5ed063f210482f8c78c72fe207e0f9da
harsh0203 <harshs0203@gmail.com>
2020-05-10 19:29:17 +0530

    Getting First Location

--- app/src/main/java/com/harshs/memorableplaces/MainActivity.java
+++ app/src/main/java/com/harshs/memorableplaces/MainActivity.java
@@ -2,7 +2,10 @@ package com.harshs.memorableplaces;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -16,11 +19,22 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         ListView listView= (ListView) findViewById(R.id.listView);
+        final Intent intent =new Intent(getApplicationContext(),MapsActivity.class);
         ArrayList<String> arrayList =  new ArrayList<String>();
         arrayList.add("Add a New Place");
         ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, arrayList);
         listView.setAdapter(arrayAdapter);
 
-        
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                intent.putExtra("placeNumber",position);
+                startActivity(intent);
+
+            }
+        });
+
+
     }
 }
--- app/src/main/java/com/harshs/memorableplaces/MapsActivity.java
+++ app/src/main/java/com/harshs/memorableplaces/MapsActivity.java
@@ -1,19 +1,56 @@
 package com.harshs.memorableplaces;
 
+import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.FragmentActivity;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+    LocationManager locationManager;
+    LocationListener locationListener;
+
+    public void zoomOnUsersLocation(Location location, String title){
+
+        LatLng usersLocation = new LatLng(location.getLatitude(),location.getLongitude());
+        mMap.clear();
+        mMap.addMarker(new MarkerOptions().position(usersLocation).title(title).icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_VIOLET)));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(usersLocation,10));
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+            if(ContextCompat.checkSelfPermission(this,Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED){
+
+                locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,0,0,locationListener);
+                Location lastKnownLocation = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+                zoomOnUsersLocation(lastKnownLocation,"Your Location....");
+
+            }
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,9 +76,45 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        Intent intent = getIntent();
+       if( intent.getIntExtra("placeNumber", 0) == 0){
+           //Zoom in onto user's location.
+           locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+           locationListener = new LocationListener() {
+               @Override
+               public void onLocationChanged(Location location) {
+
+                   zoomOnUsersLocation(location,"Your Location....");
+
+               }
+
+               @Override
+               public void onStatusChanged(String provider, int status, Bundle extras) {
+
+               }
+
+               @Override
+               public void onProviderEnabled(String provider) {
+
+               }
+
+               @Override
+               public void onProviderDisabled(String provider) {
+
+               }
+           };
+
+           if(ContextCompat.checkSelfPermission(this,Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+
+               ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION},1);
+
+           }else{
+               locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,0,0,locationListener);
+               Location lastKnownLocation = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+               zoomOnUsersLocation(lastKnownLocation,"Your Location....");
+
+           }
+       }
+
     }
 }
