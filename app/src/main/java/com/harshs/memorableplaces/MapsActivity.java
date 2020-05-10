c9ed8ead7940c616d837c9c3207d733c143489fe
harsh0203 <harshs0203@gmail.com>
2020-05-10 22:41:04 +0530

    Adding new location

--- app/src/main/java/com/harshs/memorableplaces/MapsActivity.java
+++ app/src/main/java/com/harshs/memorableplaces/MapsActivity.java
@@ -9,6 +9,8 @@ import android.Manifest;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
@@ -23,19 +25,22 @@ import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+import java.util.List;
+import java.util.Locale;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback, GoogleMap.OnMapLongClickListener {
 
     private GoogleMap mMap;
     LocationManager locationManager;
     LocationListener locationListener;
 
     public void zoomOnUsersLocation(Location location, String title){
-
-        LatLng usersLocation = new LatLng(location.getLatitude(),location.getLongitude());
-        mMap.clear();
-        mMap.addMarker(new MarkerOptions().position(usersLocation).title(title).icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_VIOLET)));
-        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(usersLocation,10));
-
+        if(location != null) {
+            LatLng usersLocation = new LatLng(location.getLatitude(), location.getLongitude());
+            mMap.clear();
+            mMap.addMarker(new MarkerOptions().position(usersLocation).title(title).icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_VIOLET)));
+            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(usersLocation, 12));
+        }
     }
 
     @Override
@@ -62,20 +67,10 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mapFragment.getMapAsync(this);
     }
 
-
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
-
+        mMap.setOnMapLongClickListener(this);
         Intent intent = getIntent();
        if( intent.getIntExtra("placeNumber", 0) == 0){
            //Zoom in onto user's location.
@@ -116,5 +111,24 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
            }
        }
 
+    }
+    @Override
+    public void onMapLongClick(LatLng latLng) {
+        Geocoder geocoder = new Geocoder(getApplicationContext(), Locale.getDefault());
+        String address="";
+        try{
+            List<Address> listAddress = geocoder.getFromLocation(latLng.latitude,latLng.longitude,1);
+            if(listAddress != null && listAddress.size() > 0){
+             if(listAddress.get(0).getThoroughfare() != null){
+                 if(listAddress.get(0).getSubThoroughfare() != null){
+                     address += listAddress.get(0).getSubThoroughfare()+" ";
+                 }
+                 address += listAddress.get(0).getThoroughfare()+" ";
+             }
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        mMap.addMarker(new MarkerOptions().position(latLng).title(address).icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_ROSE)));
     }
 }
