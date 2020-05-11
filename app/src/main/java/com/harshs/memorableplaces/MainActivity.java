792f4e147361a982a894f0d1b7e721c4b7a4c0eb
harsh0203 <harshs0203@gmail.com>
2020-05-11 19:44:54 +0530

    Saving data

--- app/src/main/java/com/harshs/memorableplaces/MainActivity.java
+++ app/src/main/java/com/harshs/memorableplaces/MainActivity.java
@@ -2,7 +2,9 @@ package com.harshs.memorableplaces;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
@@ -24,20 +26,51 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+       /* SharedPreferences sharedPreferences=this.getSharedPreferences("com.harshs.memorableplaces", Context.MODE_PRIVATE);
+
+        ArrayList<String> latitudes = new ArrayList<String>();
+        ArrayList<String> longitudes = new ArrayList<String>();
+
+        arrayList.clear();
+        latitudes.clear();
+        longitudes.clear();
+        locationList.clear();
+
+        try{
+
+            arrayList= (ArrayList<String>) ObjectSerializer.deserialize(sharedPreferences.getString("places",ObjectSerializer.serialize(new ArrayList<String>())));
+            latitudes= (ArrayList<String>) ObjectSerializer.deserialize(sharedPreferences.getString("lat",ObjectSerializer.serialize(new ArrayList<String>())));
+            longitudes= (ArrayList<String>) ObjectSerializer.deserialize(sharedPreferences.getString("lon",ObjectSerializer.serialize(new ArrayList<String>())));
+
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+
+        if(arrayList.size() > 0 && latitudes.size() > 0 && longitudes.size() > 0){
+            if(arrayList.size() == latitudes.size() && arrayList.size() == longitudes.size()){
+
+                for(int i= 0; i < latitudes.size(); i++){
+                    locationList.add(new LatLng(Double.parseDouble(latitudes.get(i)),Double.parseDouble(longitudes.get(i))));
+                }
+
+            }
+        }else{
+            arrayList.add("Add a New Place");
+            locationList.add(new LatLng(0,0));
+         }*/
+
         ListView listView= (ListView) findViewById(R.id.listView);
-        final Intent intent =new Intent(getApplicationContext(),MapsActivity.class);
+
        arrayList =  new ArrayList<String>();
        locationList = new ArrayList<LatLng>();
 
-        arrayList.add("Add a New Place");
-        locationList.add(new LatLng(0,0));
         arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, arrayList);
         listView.setAdapter(arrayAdapter);
 
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-
+                final Intent intent =new Intent(getApplicationContext(),MapsActivity.class);
                 intent.putExtra("placeNumber",position);
                 startActivity(intent);
 
